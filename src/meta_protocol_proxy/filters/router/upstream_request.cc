2b27399267c90b148a1d17b9708f114e4c36041b
gaohongbin <gaohongbin@pku.edu.cn>
2023-09-27 20:43:04 +0800

    test

--- src/meta_protocol_proxy/filters/router/upstream_request.cc
+++ src/meta_protocol_proxy/filters/router/upstream_request.cc
@@ -92,11 +92,13 @@ void UpstreamRequest::releaseUpStreamConnection(bool close) {
   ENVOY_LOG(debug, "meta protocol upstream request: release upstream connection");
   // 增加 conn_data->connection() 判断
   if (close && conn_data != nullptr) {
-      conn_data->connection();
-      ENVOY_LOG(debug, "meta protocol upstream request: 判断是否 connection() 报错");
+    try {
       // we shouldn't close the upstream connection unless explicitly asked at some exceptional cases
       conn_data->connection().close(Network::ConnectionCloseType::NoFlush);
       ENVOY_LOG(warn, "meta protocol upstream request: close upstream connection");
+    } catch (const Exception& ex) {
+      ENVOY_LOG(error, "meta protocol error: {}", ex.what());
+    }
   }
 }
 
